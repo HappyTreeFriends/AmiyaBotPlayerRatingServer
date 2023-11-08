dff33b076383ce15d8649f7e5ba1dde56e552f19
Songyang.Huo <hsyhhssyy@gmail.com>
2023-11-08 10:20:58 +0800

    没事干

---
+++ AmiyaBotPlayerRatingServer/Migrations/20231108021912_sys.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AmiyaBotPlayerRatingServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class sys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSystemGenerated",
+                table: "MAATasks",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ParentTaskId",
+                table: "MAATasks",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MAATasks_ParentTaskId",
+                table: "MAATasks",
+                column: "ParentTaskId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MAATasks_MAATasks_ParentTaskId",
+                table: "MAATasks",
+                column: "ParentTaskId",
+                principalTable: "MAATasks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MAATasks_MAATasks_ParentTaskId",
+                table: "MAATasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MAATasks_ParentTaskId",
+                table: "MAATasks");
+
+            migrationBuilder.DropColumn(
+                name: "IsSystemGenerated",
+                table: "MAATasks");
+
+            migrationBuilder.DropColumn(
+                name: "ParentTaskId",
+                table: "MAATasks");
+        }
+    }
+}
