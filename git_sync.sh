ac14d48f64e5a5a5224146f5b9a45a566534db92
shuo <songyang.huo@intersystems.com>
2024-05-22 13:14:27 +0800

    update sync script

--- git_sync.sh
+++ git_sync.sh
@@ -2,8 +2,8 @@
 
 git remote -v
 
-git push -u homecloud master
 git pull --set-upstream homecloud master
-
-git push -u origin master
 git pull --set-upstream origin master
+
+git push -u homecloud master
+git push -u origin master
